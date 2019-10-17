cad7b509ea5a0e2ca421719dc578640c3d483e9b
marishka-fishka <ya.m.l.12ym@gmail.com>
2019-10-17 12:38:27 -0400

    HW2 CSS locators

---
+++ features/locators2.py
@@ -0,0 +1,14 @@
+***
+Amazon logo---$$("div i.a-icon a-icon-logo")
+Create account field---$$("h1[class=a-spacing-small]")
+Name field---$$("input#ap_customer_name")
+Email field---$$("input[id^=ap_email]")
+Password field---$$("input#ap_password")
+Password must be at least 6 characters---$$("div[class$='Passwords must be at least 6 characters.']")
+Re-enter password field---$$("input#ap_password_check")
+Create your Amazon account button---$$("input#continue")
+Conditions of use link---$$("a[hreaf*='Conditions of Use']")
+Privacy Notice link---$$("a[hreaf*='Privacy Notice']")
+Sign in link---$$("div a[class=a-link-emphasis]")
+
+***
