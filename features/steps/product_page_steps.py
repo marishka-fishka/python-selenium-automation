83a871b65e167d987b2ced9ffb4de33f004fe1e8
marishka-fishka <ya.m.l.12ym@gmail.com>
2019-10-24 21:29:37 -0400

    HW5-Test case that loops through colors

--- features/steps/product_page_steps.py
+++ features/steps/product_page_steps.py
@@ -8,6 +8,9 @@ ADD_CART_BUTTON = (By.CSS_SELECTOR, "input#add-to-cart-button")
 CLOSING_X_SIDE_SECTION = (By.ID, 'attach-close_sideSheet-link')
 
 
+JEANS_COLORS = (By.CSS_SELECTOR, 'div#variation_color_name li')
+SELECTED_JEANS_COLOR = (By.CSS_SELECTOR,'div#variation_color_name span.selection')
+
 @when('Click Add to cart button')
 def add_cart_button(context):
     context.driver.find_element(*ADD_CART_BUTTON).click()
@@ -19,3 +22,13 @@ def close_side_section(context):
     closing_btn = context.driver.find_elements(*CLOSING_X_SIDE_SECTION)
     if len(closing_btn) == 1:
         closing_btn[0].click()
+
+
+@then('Verify user can select through jeans colors')
+def verify_jeans_colors(context):
+    expected_colors = ['Medium Wash', 'Dark Wash', 'Rinse']
+    color_webelements = context.driver.find_elements(*JEANS_COLORS)
+    for color in color_webelements:
+        color.click()
+        actual_color = context.driver.find_element(*SELECTED_JEANS_COLOR).text
+        assert actual_color == expected_colors[color_webelements.index(color)]
---
+++ features/tests/jeans_selection.feature
@@ -0,0 +1,7 @@
+# Created by Mary at 10/24/2019
+Feature: Test for jeans selection
+  # Enter feature description here
+
+  Scenario: User can loop through jeans colors
+    Given Open Amazon product B07BKD8JCQ
+    Then Verify user can select through jeans colors
