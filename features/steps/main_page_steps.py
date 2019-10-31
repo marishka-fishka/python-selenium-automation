45cdf0cba6d7dc4f3d531210ff2c3ea6f6437a7c
marishka-fishka <ya.m.l.12ym@gmail.com>
2019-10-31 10:33:06 -0400

    update environment settings

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -1,6 +1,7 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
 
 ORDERS_LINK = (By.CSS_SELECTOR, "a#nav-orders span.nav-line-2")
 SEARCH_INPUT = (By.ID, 'twotabsearchtextbox')
@@ -9,11 +10,16 @@ HAM_MENU = (By.ID, 'nav-hamburger-menu')
 AMAZON_MUSIC_MENU_ITEM = (By.XPATH, "//ul[contains(@class, 'menu-visible')]//div[contains(text(), 'Amazon Music')]")
 AMAZON_MUSIC_MENU_ITEM_RESULTS = (By.CSS_SELECTOR, 'ul.hmenu-visible a:not(.hmenu-back-button)')
 SEARCH_FIRST_PRODUCT_RESULTS = (By.XPATH, "//span[@class='a-size-base-plus a-color-base a-text-normal']")
-
+DEALS_UNDER_25_LINK=(By.XPATH, "//a[contains(@aria-label, 'deals under $25')]")
 
 @given('Open Amazon page')
 def open_amazon(context):
     context.driver.get('https://www.amazon.com')
+
+@given('Open Amazon wholefoods page')
+def open_wholeffods_page(context):
+    context.driver.get('https://www.amazon.com/wholefoodsdeals')
+
 
 
 @when('Click Amazon Orders link')
@@ -50,3 +56,46 @@ def verify_amount_of_items(context):
     print(len(context.driver.find_elements(*AMAZON_MUSIC_MENU_ITEM_RESULTS)))
     assert len(context.driver.find_elements(*AMAZON_MUSIC_MENU_ITEM_RESULTS)) == 6, \
         f'Expected 6 items but got {len(context.driver.find_elements(*AMAZON_MUSIC_MENU_ITEM_RESULTS))}'
+
+
+
+
+@when('Store original windows')
+def store_current_windows(context):
+    context.original_window = context.driver.current_window_handle
+    context.old_windows = context.driver.window_handles
+    print('original_window',  context.original_window)
+    print('old_window', context.old_windows)
+
+@when('Click to open Deals under 25')
+def click_to_open_deals_under_25(context):
+    context.driver.find_element(*DEALS_UNDER_25_LINK).click()
+
+@when('Switch to the newly opened window')
+def switch_to_new_window(context):
+    # wait for new window
+    context.driver.wait.until(EC.new_window_is_opened)
+
+    current_windows = context.driver.window_handles
+    print('\n"current_windows"\n' , current_windows)
+
+    #new_window = current_windows[1]
+    #print('new_window', new_window)
+
+    new_windows = current_windows
+    for old_window in context.old_windows:
+        new_windows.remove(old_window)
+    print('new_windows', new_windows)
+    # Switch to freshly opened window
+    context.driver.switch_to_window(new_windows[0])
+
+@then('User can close new window and switch back to original')
+def close_and_switch_window_back(context):
+    context.driver.close()
+    context.driver.switch_to_window(context.original_window)
+    sleep(4)
+
+@when('Refresh the page')
+def refresh_the_page(context):
+    context.driver.refresh()
+    sleep(4)
--- features/steps/product_results_page_steps.py
+++ features/steps/product_results_page_steps.py
@@ -1,6 +1,8 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+
 
 TOOLBAR_TEXT_BOLD = (By.CSS_SELECTOR, "h1 span.a-text-bold")
 CARD_ITEM_COUNT = (By.ID, 'nav-cart-count')
@@ -18,3 +20,14 @@ def verify_number_of_item(context, expected_number):
     actual_number_of_items = len(context.driver.find_element(*CARD_ITEM_COUNT).text)
     assert actual_number_of_items == int(
         expected_number), f'Expected {expected_number}, but got {actual_number_of_items}'
+
+
+@when('Put item in the cart')
+def put_item_in_cart(context):
+   context.driver.find_element(By.CSS_SELECTOR, "a[id='dealTitle']").click()
+   context.driver.wait.until(EC.element_to_be_clickable)
+   context.driver.find_element(By.ID, 'add-to-cart-button').click()
+   context.driver.wait.until(EC.element_to_be_clickable)
+   context.driver.find_element(By.CSS_SELECTOR, "button[id='attachSiAddCoverage-announce']").click()
+
+
